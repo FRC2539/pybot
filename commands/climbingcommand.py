369b0951a4e1e218f23be21a06c8a7b9a90f6ec7
Krypton Cougars <kryptoncougars@pasd.us>
2017-02-02 18:37:38 -0500

    Added climbing command.

---
+++ commands/climbingcommand.py
@@ -0,0 +1,18 @@
+from wpilib.command import Command
+
+import subsystems
+
+class PickupCommand(Command):
+    # Initialize the named command.
+    def __init__(self):
+        super().__init__('PickupCommand')
+
+        self.requires(subsystems.climber)
+
+    def initialize(self):
+        subsystems.climber.startClimbing()
+
+    def isFinished(self):
+        return subsystems.climber.atTop()
+    def end(self):
+        subsystems.climber.stop()
