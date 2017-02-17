24c3c0c7c3f1c7291ec551995160ccc02a928a62
Bob Stouffer <bob@kryptoncougars.pasd.us>
2017-02-17 16:06:50 -0500

    Allows setting drive speed by command

--- commands/gear/hanggearcommandgroup.py
+++ commands/gear/hanggearcommandgroup.py
@@ -1,9 +1,5 @@
 from wpilib.command import CommandGroup
 
-from custom.config import Config
-
-from ..drive.movecommand import MoveCommand
-from ..drive.turncommand import TurnCommand
 from commands.gear.gotoliftcommand import GoToLiftCommand
 from commands.gear.turntoliftcommand import TurnToLiftCommand
 
