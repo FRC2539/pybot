c068d37efc8021a796db8f3f19e06d1e99e744e0
Krypton Cougars <latitude1@kryptoncougars.pasd.us>
2017-06-12 20:24:52 -0400

    Added a check for DriveTrain PIDs to the SystemCheck command.

--- commands/test/systemCheck.py
+++ commands/test/systemCheck.py
@@ -8,26 +8,27 @@ class SystemCheck(Command):
 
     def initialize(self):
         # Print information about connection to each system
-        print("Connection to system\n")
+        print("Firmware Version\n")
         # Drivetrain
-        print("DriveTrain\n")
-        print("Front Left: %s" % (CANTalon(ports.drivetrain.frontLeftMotorID).getFirmwareVersion()))
-        print("Front Right: %s" % (CANTalon(ports.drivetrain.frontRightMotorID).getFirmwareVersion()))
-        print("Back Left: %s" % (CANTalon(ports.drivetrain.backLeftMotorID).getFirmwareVersion()))
-        print("Back Right: %s\n" % (CANTalon(ports.drivetrain.backRightMotorID).getFirmwareVersion()))
+        print("Front Left DriveTrain: Version %s" % (CANTalon(ports.drivetrain.frontLeftMotorID).getFirmwareVersion()))
+        print("Front Right DriveTrain: Version %s" % (CANTalon(ports.drivetrain.frontRightMotorID).getFirmwareVersion()))
+        print("Back Left DriveTrain: Version %s" % (CANTalon(ports.drivetrain.backLeftMotorID).getFirmwareVersion()))
+        print("Back Right Drivetrain: Version %s\n" % (CANTalon(ports.drivetrain.backRightMotorID).getFirmwareVersion()))
+
         # Shooter
-        print("Shooter\n")
-        print(CANTalon(ports.shooter.motorID).getFirmwareVersion())
+        print("Shooter: Version %s" % (CANTalon(ports.shooter.motorID).getFirmwareVersion()))
         print()
         # Pickup
-        print("Pickup\n")
-        print(CANTalon(ports.pickup.motorID).getFirmwareVersion())
+        print("pickup: Version %s" % (CANTalon(ports.pickup.motorID).getFirmwareVersion()))
         print()
         # Climber
-        print("Climber\n")
-        print(CANTalon(ports.climber.motorID).getFirmwareVersion())
+        print("Climber: Version %s" % (CANTalon(ports.climber.motorID).getFirmwareVersion()))
 
-        # Print PID values of each motor.
+        print("Motor PIDs")
+        leftDriveTrain = subsystems.drivetrain.activeMotors[0]
+        rightDriveTrain = subsystems.drivetrain.activeMotors[1]
+        print("Left DriveTrain: %s, %s, %s" % (leftDriveTrain.getP, leftDriveTrain.getI, leftDriveTrain.getD))
+        print("Right DriveTrain: %s, %s, %s" % (rightDriveTrain.getP, rightDriveTrain.getI, rightDriveTrain.getD))
         # Print speed of shooter.
         # Print position of each encoder
         # Print velocity of each encoder.
