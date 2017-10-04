10ca942655ec7982db58755fe9a5d98c5a27c05f
Krypton Cougars <kryptoncougars@pasd.us>
2017-10-03 21:04:37 -0400

    I made changes to make the autonomous program so that it moves fluently.

--- commands/autonomouscommandgroup.py
+++ commands/autonomouscommandgroup.py
@@ -1,13 +1,14 @@
 from wpilib.command.commandgroup import CommandGroup
+from wpilib.command.waitcommand import WaitCommand
 import custom.flowcontrol as fc
 import subsystems
+
 from commands.gear.scoregearcommandgroup import ScoreGearCommandGroup
 from commands.gear.waitonpilotcommand import WaitOnPilotCommand
 from commands.drive.movecommand import MoveCommand
 from wpilib.driverstation import DriverStation
 from commands.drive.turncommand import TurnCommand
 from commands.shooter.firecommand import FireCommand
-from commands.alertcommand import AlertCommand
 from custom.config import Config
 
 
@@ -48,18 +49,42 @@ class AutonomousCommandGroup(CommandGroup):
 
             @fc.IF(lambda: ds.getAlliance() == ds.Alliance.Red)
             def turnRight(self):
-                self.addSequential(MoveCommand(15))
-                self.addSequential(AlertCommand("Step 1"))
-                self.addSequential(TurnCommand(5))
-                self.addSequential(MoveCommand(20))
-
+                self.addSequential(MoveCommand(25))
+                self.addSequential(WaitCommand(0.1))
+                self.addSequential(TurnCommand(25))
+                self.addSequential(MoveCommand(10))
+                self.addSequential(TurnCommand(70))
 
             @fc.ELSE
             def turnLeft(self):
-                self.addSequential(MoveCommand(10))
-                self.addSequential(TurnCommand(-90))
-                self.addSequential(MoveCommand(15))
-                self.addSequential(TurnCommand(90))
+                self.addSequential(MoveCommand(-25))
+                self.addSequential(WaitCommand(0.1))
+                self.addSequential(TurnCommand(-25))
+                self.addSequential(MoveCommand(-10))
+                self.addSequential(TurnCommand(-70))
+
+            @fc.IF(lambda: subsystems.gear.isLiftVisible())
+            def centerPosition(self):
+                @fc.IF(lambda: subsystems.gear.hasGear())
+                def hangCenter(self):
+                    self.addSequential(ScoreGearCommandGroup())
+                    self.addSequential(WaitOnPilotCommand())
+                    self.addSequential(MoveCommand(-10))
+
+                @fc.IF(lambda: ds.getAlliance() == ds.Alliance.Red)
+                def turnLeft(self):
+                    self.addSequential(TurnCommand(-90))
+                    self.addSequential(MoveCommand(50))
+                    self.addSequential(TurnCommand(90))
+                    self.addSequential(MoveCommand(240))
+
+                @fc.ELSE
+                def turnRight(self):
+                    self.addSequential(TurnCommand(90))
+                    self.addSequential(MoveCommand(50))
+                    self.addSequential(TurnCommand(-90))
+                    self.addSequential(MoveCommand(240))
+
 
         @fc.ELSE
         def sides(self):
