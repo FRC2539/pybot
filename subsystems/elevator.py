6c2b933019217b2e0e5ebdfdc3fc5c305dc56c40
Krypton Cougars <latitude1@kryptoncougars.pasd.us>
2018-02-26 18:43:58 -0500

    Changed the elevator.

--- subsystems/elevator.py
+++ subsystems/elevator.py
@@ -19,7 +19,7 @@ class Elevator(Subsystem):
         )
 
         self.lowerLimit = 0
-        self.upperLimit = 21600
+        self.upperLimit = 19000
         self.motor.configReverseSoftLimitEnable(True, 0)
         self.motor.configForwardSoftLimitEnable(True, 0)
         self.motor.configReverseSoftLimitThreshold(self.lowerLimit, 0)
