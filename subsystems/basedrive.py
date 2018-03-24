0ea32572ea5187db10f5e13da825e4ab9b61e53b
Krypton Cougars <kryptoncougars@pasd.us>
2018-03-24 13:25:57 -0400

    Tweaked gyro calculations. And adjusted some autonomous accordingly.

--- subsystems/basedrive.py
+++ subsystems/basedrive.py
@@ -163,15 +163,15 @@ class BaseDrive(DebuggableSubsystem):
             raise RuntimeError('Cannot set position. Encoders are disabled.')
 
         index = 0
-        angle = subsystems.drivetrain.getAngle()
+        angle = subsystems.drivetrain.getAngle() * 75
 
         for motor, position in zip(self.activeMotors, positions):
             motor.selectProfileSlot(1, 0)
-            if index == 0 and angle > 2.0:
-                motor.configMotionCruiseVelocity(int(self.speedLimit - (angle * 75)), 0)
+            if index == 0 and angle > 1:
+                motor.configMotionCruiseVelocity(int(self.speedLimit - angle), 0)
 
-            elif index == 1 and angle < -2.0:
-                motor.configMotionCruiseVelocity(int(self.speedLimit + (angle * 75)), 0)
+            elif index == 1 and angle < -1:
+                motor.configMotionCruiseVelocity(int(self.speedLimit + angle), 0)
 
             else:
                 motor.configMotionCruiseVelocity(int(self.speedLimit), 0)
