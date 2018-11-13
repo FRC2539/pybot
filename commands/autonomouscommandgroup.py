ce916a9bb47ac7ede3eb14803686a623d26e3567
Krypton Cougars <latitude1@kryptoncougars.pasd.us>
2018-11-13 18:12:23 -0500

    Preliminary pathfinder v1 testing.

--- commands/autonomouscommandgroup.py
+++ commands/autonomouscommandgroup.py
@@ -3,68 +3,56 @@ import commandbased.flowcontrol as fc
 from custom.config import Config
 
 from wpilib.command.waitcommand import WaitCommand
-from commands.drivetrain.pivotcommand import PivotCommand
-from commands.drivetrain.movecommand import MoveCommand
-from commands.drivetrain.turncommand import TurnCommand
-from commands.drivetrain.turntocommand import TurnToCommand
-from commands.drivetrain.pivottocommand import PivotToCommand
-from commands.drivetrain.zerogyrocommand import ZeroGyroCommand
-from commands.intake.intakecommand import IntakeCommand
-from commands.intake.outakecommand import OutakeCommand
-from commands.elevator.gotoheightcommand import GoToHeightCommand
-from commands.resetcommand import ResetCommand
-from commands.elevator.elevatorresetcommand import ElevatorResetCommand
-from commands.drivetrain.setspeedcommand import SetSpeedCommand
-from commands.network.alertcommand import AlertCommand
-from wpilib.command.printcommand import PrintCommand
+import pathfinder as pf
+import math
 
 class AutonomousCommandGroup(CommandGroup):
 
     def __init__(self):
         super().__init__('Autonomous')
 
-        self.addSequential(ZeroGyroCommand())
+        import pathfinder as pf
+        import math
 
-        @fc.IF(lambda: True)
-        def phase1(self):
-            self.addParallel(GoToHeightCommand(5650))
+        if __name__ == '__main__':
 
-#NOTE:  To Ben, Your problem was that you set the upper limit of the elevator subsystem too low Sincerely, Quentin
+            points = [
+            pf.Waypoint(-4, -1, math.radians(-45.0)),
+            pf.Waypoint(-2, -2, 0),
+            pf.Waypoint(0, 0, 0),
+            ]
 
-            self.addSequential(WaitCommand(1))
-            self.addParallel(MoveCommand(36))
+            info, trajectory = pf.generate(points, pf.FIT_HERMITE_CUBIC, pf.SAMPLES_HIGH,
+                                        dt=0.05, # 50ms
+                                        max_velocity=1.7,
+                                        max_acceleration=2.0,
+                                        max_jerk=60.0)
 
-        self.addSequential(WaitCommand(1))
+            # Wheelbase Width = 0.5m
+            modifier = pf.modifiers.TankModifier(trajectory).modify(0.73)
 
-        @fc.IF(lambda: True)
-        def phase2(self):
-            self.addParallel(MoveCommand(-36))
-            self.addSequential(WaitCommand(0.1))
-            self.addParallel(OutakeCommand(-0.5))
-            #self.addParallel(SetSpeedCommand(435))
-#NOTE: To Bew and Quentle, your command went pycho . . . Not my fault, Samantha
-#NOTE: To Bew and Quentle, I fixed your robot. -Maxim P.S. Your names are now Bew and Quentle
+            # Do something with the new Trajectories...
+            left = modifier.getLeftTrajectory()
+            right = modifier.getRightTrajectory()
 
-        self.addSequential(GoToHeightCommand(0))
-        self.addSequential(AlertCommand('You have been hacked......................................... System Failing............... Detected Threat............................ ISIS ................. Programming attemps............ Robot will.............. Self Destruct.............. IN'))
-        self.addSequential(WaitCommand(1))
-        self.addSequential(AlertCommand('3', 'Info'))
-        self.addSequential(WaitCommand(1))
-        self.addSequential(AlertCommand('2', 'Info'))
-        self.addSequential(WaitCommand(1))
-        self.addSequential(AlertCommand('1', 'Info'))
-        self.addSequential(WaitCommand(1))
-        self.addSequential(AlertCommand('<b style="font-size:40px">BOOM</b>'))
-        self.addSequential(SetSpeedCommand(1700))
-        self.addSequential(MoveCommand(-35))
-        self.addSequential(TurnCommand(-90))
-        self.addSequential(MoveCommand(35))
-        self.addSequential(AlertCommand('HI'))
-        self.addSequential(TurnCommand(135))
-        @fc.IF(lambda: True)
-        def phase3(self):
-            self.addParallel(MoveCommand(75.2728370662))
-            self.addParallel(IntakeCommand(0.75))
+            leftFollower = pf.followers.EncoderFollower(left)
+            leftFollower.configureEncoder(self.l_encoder.get(), self.ENCODER_COUNTS_PER_REV, self.WHEEL_DIAMETER)
+            leftFollower.configurePIDVA(1.0, 0.0, 0.0, 1 / self.MAX_VELOCITY, 0)
 
-        #self.addSequential(IntakeCommand(0.5))
-        #self.addSequential(MoveCommand(34))
+            rightFollower = pf.followers.EncoderFollower(right)
+            rightFollower.configureEncoder(self.r_encoder.get(), self.ENCODER_COUNTS_PER_REV, self.WHEEL_DIAMETER)
+            rightFollower.configurePIDVA(1.0, 0.0, 0.0, 1 / self.MAX_VELOCITY, 0)
+
+            self.leftFollower = leftFollower
+            self.rightFollower = rightFollower
+
+            print("auto done")
+
+            # This code renders the followed path on the field in simulation (requires pyfrc 2018.2.0+)
+            if wpilib.RobotBase.isSimulation():
+                from pyfrc.sim import get_user_renderer
+                renderer = get_user_renderer()
+                if renderer:
+                    renderer.draw_pathfinder_trajectory(left, color='#0000ff', offset=(-1,0))
+                    renderer.draw_pathfinder_trajectory(modifier.source, color='#00ff00', show_dt=1.0, dt_offset=0.0)
+                    renderer.draw_pathfinder_trajectory(right, color='#0000ff', offset=(1,0))
