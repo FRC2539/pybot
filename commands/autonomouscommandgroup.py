cb54e1d47c78dab44061e19f68bd2c62bd322673
Krypton Cougars <latitude1@kryptoncougars.pasd.us>
2019-01-19 10:56:16 -0500

    Vision feeds values via networktable to autonomous and other codes. Works well.

--- commands/autonomouscommandgroup.py
+++ commands/autonomouscommandgroup.py
@@ -1,9 +1,17 @@
 from wpilib.command import CommandGroup
 import commandbased.flowcontrol as fc
 from custom.config import Config
-
+from networktables import NetworkTables
+from commands.network.alertcommand import AlertCommand
 
 class AutonomousCommandGroup(CommandGroup):
 
     def __init__(self):
         super().__init__('Autonomous')
+        NetworkTables.initialize(server='roborio-2539-frc.local')
+
+        distanceToCargo = Config('cameraInfo/distanceToCargo', None)
+        hasCargo = Config('cameraInfo/cargoFound', False)
+        print(str(hasCargo))
+        if hasCargo:
+            self.addSequential(AlertCommand('Distance to cargo: %s' % float(distanceToCargo)))
