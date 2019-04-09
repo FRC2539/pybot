ab8b528fd9b68b8b67b4088d3c6ff9af8fc3f099
Krypton Cougars <latitude1@kryptoncougars.pasd.us>
2019-04-09 19:35:52 -0400

    Added some fun shell scripts.

---
+++ pullrepos.sh
@@ -0,0 +1,6 @@
+cd ~/code/pybot
+git pull
+cd ../vision
+git pull
+cd ../frcDashboard
+git pull
---
+++ pushrepos.sh
@@ -0,0 +1,15 @@
+cd ~
+cd code/pybot
+git add -A
+git commit
+git push
+
+cd ../vision
+git add -A
+git commit
+git push
+
+cd ../frcDashboard
+git add -A
+git commit
+git push
