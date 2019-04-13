8564a396cf3cc1c89591a3a419f548fde99afc8f
FRC2539 <kryptoncougars2539@gmail.com>
2019-04-13 09:42:07 -0400

    Added drive mode default for non-auto movements.

--- commands/drivetrain/gototapecommand.py
+++ commands/drivetrain/gototapecommand.py
@@ -7,7 +7,7 @@ from networktables import NetworkTables
 
 class GoToTapeCommand(Command):
 
-    def __init__(self, pipeID=0):
+    def __init__(self, pipeID=1):
         super().__init__('Go To Tape')
 
         self.requires(robot.drivetrain)
@@ -34,8 +34,6 @@ class GoToTapeCommand(Command):
 
 
     def initialize(self):
-        self.nt.putNumber('ledMode', 3)
-
         self.originallyFieldOriented = robot.drivetrain.isFieldOriented
 
         if self.originallyFieldOriented:
@@ -48,7 +46,7 @@ class GoToTapeCommand(Command):
     def execute(self):
         #old
         if self.tape.getValue() == 1:
-            oX = self.strafe.getValue() + 3.5 #Adjust for off center camera position
+            oX = self.strafe.getValue() + 1.0 #3.5 #Adjust for off center camera position
             oY = self.distance.getValue()
 
             self.x = math.copysign((oX * 4) / 100, oX)
@@ -96,59 +94,6 @@ class GoToTapeCommand(Command):
             print('No vision target found!')
             robot.drivetrain.move(0, 0, 0)
 
-        ################################################3
-        #__new__
-
-
-        #if self.tape.getValue() == 1:
-            #oX = self.strafe.getValue() + 1.0 #Adjust for off center camera position
-            #oY = self.distance.getValue()
-
-            #self.x = math.copysign((oX * 3) / 100, oX)
-            #self.y = math.copysign((oY * 3) / 100, oY)
-            #self.rotate = self.x / 2
-
-
-            #if self.x > 0.4:
-                #self.x = math.copysign(0.4, self.x)
-                #self.rotate = self.x
-            #elif abs(oX) <= 0.5:
-                #self.x = oX / 5
-                #self.rotate = self.x
-            #elif abs(oX) > 0.5 and self.x < 0.1:
-                #self.x = math.copysign(0.23, oX)
-                #self.rotate = math.copysign(0.12, oX)
-
-            ##if self.y > 0.45:
-             ##   self.y = 0.5
-            #if oY < 2.5:
-                #self.y = 0.15
-            #elif oY < 0.0:
-                #self.y = 0
-            #elif oY > 0.8 and self.y < 0.15:
-                #self.y = 0.2
-            #elif oY > 4.5:
-                #print('running bens stuff')
-                #self.y = 0.6
-
-            #self.nx = self.x * self.fineStrafe
-            #self.nRotate = self.rotate * self.fineRotate
-            #self.ny = self.y * self.fineMove
-
-            #robot.drivetrain.move(self.nx, self.ny, self.nRotate)
-
-            #self._finished = (abs(self.x) <= 0.02 and abs(self.y) <= 0.02 and abs(self.rotate) <= 0.02) or oY <= 0.25
-
-            #if self._finished:
-                #robot.lights.solidGreen()
-            #else:
-                #robot.lights.solidPurple()
-
-        #else:
-            #robot.lights.solidRed()
-            #print('No vision target found!')
-            #robot.drivetrain.move(0, 0, 0)
-
 
     def isFinished(self):
         return self._finished
@@ -161,7 +106,5 @@ class GoToTapeCommand(Command):
 
         if self.originallyFieldOriented:
             robot.drivetrain.toggleFieldOrientation()
-
-        self.nt.putNumber('ledMode', 1)
 
         robot.lights.off()
