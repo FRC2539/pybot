46e9d565b3fed73a0354ee28d5f4dc8c4c7014cc
Krypton Cougars <macbook1@kryptoncougars.pasd.us>
2019-12-13 20:36:05 -0500

    Worked a lot on the controller for the driver, but even more for the operator. I really hope I am done (spoiler alert, I'm not!). Controls should all be fully functional and versatile; however, need to look into the DPad usage.

--- controller/buildlayout.py
+++ controller/buildlayout.py
@@ -6,7 +6,7 @@ from wpilib.interfaces import GenericHID
 
 class BuildLayout:
     def __init__(self, driver, operator, funcsD, funcsO):
-        self.controllerUno = XboxController(driver) # LogitechDualshock(_id) Make sure the controller is in Xinput mode.
+        self.controllerUno = XboxController(driver) # LogitechDualshock(_id) Make sure the controller is in Xinput mode. Also, 0 is left, and 1 is right
         self.controllerDos = XboxController(operator)
 
         self.buttonID = LogitechDualshock
@@ -14,10 +14,82 @@ class BuildLayout:
         self.functionsD = funcsD
         self.functionsO = funcsO
 
-        self.buttonsToXboxDriver = {'A' : self.controllerUno.getAButtonPressed}
+        self.commandDr = None
+        self.commandOp = None
+
+        self.buttonsToXboxDriver = {
+                                    'A' : self.controllerUno.getAButtonPressed,
+                                    'X' : self.controllerUno.getXButtonPressed,
+                                    'Y' : self.controllerUno.getYButtonPressed,
+                                    'B' : self.controllerUno.getBButtonPressed,
+                                    'Back' : self.controllerUno.getBackButtonPressed,
+                                    'Start' : self.controllerUno.getStartButtonPressed,
+                                    'LeftTrigger' : self.getLeftTriggerDriver,              # Use this for a shoot or something. Use axis elsewhere.
+                                    'RightTrigger' : self.getRightTriggerDriver,            # Same as above.
+                                    'LeftBumper' : self.controllerUno.getBumperPressed(0),
+                                    'RightBumper' : self.controllerUno.getBumperPressed(1),
+                                    'LeftStick' : self.controllerUno.getStickButtonPressed(0),
+                                    'RightStick' : self.controllerUno.getStickButtonPressed(1)
+                                    }
+
+        self.buttonsToXboxOp = {
+                                'A' : self.controllerDos.getAButtonPressed,
+                                'X' : self.controllerDos.getXButtonPressed,
+                                'Y' : self.controllerDos.getYButtonPressed,
+                                'B' : self.controllerDos.getBButtonPressed,
+                                'Back' : self.controllerDos.getBackButtonPressed,
+                                'Start' : self.controllerDos.getStartButtonPressed,
+                                'LeftTrigger' : self.getLeftTriggerOp,              # Use this for a shoot or something. Use axis elsewhere.
+                                'RightTrigger' : self.getRightTriggerOp,            # Same as above.
+                                'LeftBumper' : self.controllerDos.getBumperPressed(0),
+                                'RightBumper' : self.controllerDos.getBumperPressed(1),
+                                'LeftStick' : self.controllerDos.getStickButtonPressed(0),
+                                'RightStick' : self.controllerDos.getStickButtonPressed(1)
+                                }
+
+        # TODO incorporate the dpad.
+
+    ''' The following are for trigger bool statements '''
+    def getRightTriggerDriver(self):
+        if self.controllerUno.getTriggerAxis(1) > 0.05:
+            return True
+        else:
+            return False
+
+    def getLeftTriggerDriver(self):
+        if self.controllerUno.getTriggerAxis(0) > 0.05:
+            return True
+        else:
+            return False
+    def getRightTriggerOp(self):
+        if self.controllerDos.getTriggerAxis(1) > 0.05:
+            return True
+        else:
+            return False
+    def getLeftTriggerOp(self):
+        if self.controllerDos.getTriggerAxis(0) > 0.05:
+            return True
+        else:
+            return False
+
+
+    ''' The following are for trigger scaling statements. '''
+
+    def getRightTriggerAxisDriver(self):
+        return self.controllerUno.getTriggerAxis(1)
+
+    def getLeftTriggerAxisDriver(self):
+        return self.controllerUno.getTriggerAxis(0)
+
+    def getRightTriggerAxisOp(self):
+        return self.controllerDos.getTriggerAxis(1)
+
+    def getLeftTriggerAxisOp(self):
+        return self.controllerDos.getTriggerAxis(0)
+
 
     def getX(self):
-        return self.controllerUno.getX(0) # 0 is left, 1 is right
+        return self.controllerUno.getX(0)
 
     def getY(self):
         return self.controllerUno.getY(0)
@@ -26,6 +98,7 @@ class BuildLayout:
         return self.controllerUno.getX(1)
 
     def checkEarly(self):
+        ''' Checks for valid inputs '''
         for func in self.functionsD:
             for var in func:
                 if not type(var) is str:
@@ -36,10 +109,17 @@ class BuildLayout:
                     raise TypeError('Check functions list for operator in robot.py. They need to be valid strings; We got "' + str(var) + '" in "' + str(func) + '".')
 
     def checkDriver(self):
+        ''' Checks for driver action '''
         for func in self.functionsD: # Takes given functions, takes command from buttonsToXbox, and watches it.
-            print(func)
-            self.command = self.buttonsToXboxDriver[func[0]]
-            if self.command():
+            self.commandDr = self.buttonsToXboxDriver[func[0]]
+            if self.commandDr(): # Checks to see if returns true. This will NOT work with scaling triggers!
                 return func[1], func[2]
         return False, False
 
+    def checkOperator(self):
+        ''' Checks for operator action '''
+        for func in self.functionsO: # Takes given functions, takes command from buttonsToXbox, and watches it.
+            self.commandOp = self.buttonsToXboxOp[func[0]]
+            if self.commandOp(): # Checks to see if returns true. This will NOT work with scaling triggers!
+                return func[1], func[2]
+        return False, False
