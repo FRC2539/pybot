bc6094261be365dd8a23078f51dbf3a44aee8c20
Krypton Cougars <macbook1@kryptoncougars.pasd.us>
2020-01-02 17:56:59 -0500

    Finalized stuff for more the motor controller.

--- subsystems/motors.py
+++ subsystems/motors.py
@@ -1,5 +1,7 @@
 from .debuggablesubsystem import DebuggableSubsystem
 
+from wpilib import DigitalInput
+
 import ports
 import robot
 
@@ -8,11 +10,17 @@ from rev import CANSparkMax, MotorType
 
 class Motors(DebuggableSubsystem):
     '''Describe what this subsystem does.'''
-
+    # NOTE: The get() is inverted in DI/O
     def __init__(self):
         super().__init__('Motors')
         self.talon = WPI_TalonSRX(2)
         self.spark = CANSparkMax(1, MotorType.kBrushless)
+
+        self.talonReverseSwitch = DigitalInput(0)
+        self.sparkReverseSwitch = DigitalInput(1)
+
+        self.talonEnabled = DigitalInput(2)
+        self.sparkEnabled = DigitalInput(3)
 
         self.talon.setNeutralMode(NeutralMode.Coast)
         self.talon.setSafetyEnabled(False)
@@ -21,9 +29,20 @@ class Motors(DebuggableSubsystem):
         percents = self.calcPercents()
 
         print('Setting Percent Output: ' + str(percents) + '.')
+        if not self.talonEnabled.get():
+            if self.talonReverseSwitch.get():
+                self.talon.set(ControlMode.PercentOutput, float(percents * -1))
+            else:
+                self.talon.set(ControlMode.PercentOutput, percents)
 
-        self.talon.set(ControlMode.PercentOutput, percents)
-        self.spark.set(percents)
+
+        if not self.sparkEnabled.get():
+            if self.sparkReverseSwitch.get():
+                self.spark.set(percents * -1)
+            else:
+                self.spark.set(percents)
+
+        print('NO MOTORS ENABLED!')
 
     def calcPercents(self):
         # MODIFY THIS AS NEEDED
