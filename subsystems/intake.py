786be7488c69f62b0d00f3191da426d9572efe39
Benjamin Bistline <bbistline02@gmail.com>
2020-01-31 18:17:56 -0500

    wrote intake earlier. o Please enter the commit message for your changes. Lines starting

---
+++ subsystems/intake.py
@@ -0,0 +1,34 @@
+import ports
+
+from .debuggablesubsystem import DebuggableSubsystem
+
+from rev import CANSparkMax, MotorType
+
+class Intake(DebuggableSubsystem):
+    def __init__(self):
+        super().__init__('Intake')
+
+        self.intakeMotor = CANSparkMax(ports.IntakePorts.motorID, MotorType.kBrushed)
+
+        self.intakeMotor.setInverted(False)
+
+        self.ballCount = 0
+
+    def intake(self):
+        self.intakeMotor.set(1)
+
+    def outake(self):
+        self.intakeMotor.set(-0.4)
+
+    def stop(self):
+        self.intakeMotor.stopMotor()
+
+    def monitorIntake(self): # Experimental
+        # Gets output current, puts it in milliamperes. The free current (of a bag) at top speed is about 1.8 amps, will spike if more resistance (ball) - see motor chart.
+        current = self.intakeMotor.getOutputCurrent()
+        print(current)
+        if current * 1000 > 1800:
+            self.ballCount += 1
+
+    def maintainBalls(self):
+        self.intakeMotor.set(0.3)
