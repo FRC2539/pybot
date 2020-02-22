c186645095142d24d496e9682bb72687f5e77094
Krypton Cougars <macbook1@kryptoncougars.pasd.us>
2020-02-22 11:09:28 -0500

    we will have a merge conflict dang

--- commands/configauto.py
+++ commands/configauto.py
@@ -3,7 +3,7 @@ from networktables import NetworkTables
 def init():
     table = NetworkTables.getTable('Autonomous')
 
-    autoNames = ['Eat Beans', 'Inner Power Port :)', 'Inner Power Port But More 8D',' SkSkSkirt off the init line', 'REEEEEEEEEE','shootie trench' ]
+    autoNames = ['Simple Shoot', '', 'Shoot, Trench, Collect 5','Shoot, Trench, Collect 3, Shoot', 'REEEEEEEEEE','shootie trench' ]
     autoString = ''
 
     for auto in autoNames:
