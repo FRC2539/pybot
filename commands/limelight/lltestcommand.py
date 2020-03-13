292a9c08e8bfa134d5163f193dd776b1663b922f
FRC2539 <kryptoncougars2539@gmail.com>
2020-03-12 20:00:56 -0400

    WOrking

--- commands/limelight/lltestcommand.py
+++ commands/limelight/lltestcommand.py
@@ -8,30 +8,45 @@ class llTestCommand(Command):
     def __init__(self):
         super().__init__('ll Test')
 
-       #self.requires(robot.limelight)
-        #self.requires(robot.drivetrain)
+        self.requires(robot.drivetrain)
         self.requires(robot.turret)
+
 
 
 
 
     def initialize(self):
         robot.limelight.setPipeline(1)
-
+        self.tx = 0
+        self.ty = 0
+        self.deltaX = 0
+        self.deltaY = 0
 
 
     def execute(self):
-        self.x = robot.limelight.getX()
-        self.rotate = self.x * -.03
-        if (abs(self.rotate) > .5):
-            self.rotate = math.copysign(.5, self.rotate)
-        robot.turret.move(self.rotate)
+        if robot.turret.getPosition() > 825 and robot.turret.getPosition() < 875:
+            self.x = robot.limelight.getX()
+            self.rotate = self.x * .05
+            if (abs(self.rotate) > .5):
+                self.rotate = math.copysign(.5, self.rotate)
+            if abs(self.rotate) < .07 and abs(self.x) > 1:
+                self.rotate = math.copysign(.07, self.rotate)
+            if abs(self.x) < 1:
+                self.rotate = 0
+                #bens move
+                self.x = robot.limelight().calcXDistance()
+                self.y = robot.limelight().calcYDistance()
+                self.deltaX = self.tx - self.x
+                self.deltaY = self.ty - self.y
+                self.angle = math.degrees(math.atan(self.deltaX/self.deltaY))
+                self.deltaD = math.sqrt(self.deltaX * self.deltaX + self.deltaY * self.deltaY)
+            else:
+                robot.drivetrain.move(0,0,self.rotate)
+        else:
+            robot.turret.setPosition(850)
 
 
-        print('Field Angle = '+str(robot.limelight.getFeildAngle()))
-        print('distance = ' + str(robot.limelight.calcDistance()))
-        print('x = '+ str(robot.limelight.calcXDistance()))
-        print('y = '+ str(robot.limelight.calcYDistance()))
+
 
 
     def end(self):
