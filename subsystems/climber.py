0c7a4d09e7609085bf9a6ee85d300fbac27748ad
Ben Bistline <bbistline02@gmail.com>
2020-10-29 20:57:38 -0400

    Made the climber and subsystem.

--- subsystems/climber.py
+++ subsystems/climber.py
@@ -1,13 +1,31 @@
 from wpilib.command import Subsystem
 
-from .cougarsystem import *
+from rev import CANSparkMax, MotorType, IdleMode
+
+from .cougarsystem import  *
 
 import ports
 
 class Climber(Subsystem):
-    '''Describe what this subsystem does.'''
+    '''Makes the climber move.'''
 
     def __init__(self):
+        
         super().__init__('Climber')
-    
+        
         disablePrints()
+
+        self.climberMotor = CANSparkMax(ports.climber.motorID, MotorType.kBrushless)
+            
+        self.climberMotor.setIdleMode(IdleMode.kBrake)
+        self.climberMotor.setInverted(False)
+    
+    def raiseClimber(self):
+       self.climberMotor.set(0.5)
+    
+    def lowerClimber(self):
+       self.climberMotor.set(-0.5)
+     
+    def stopClimber(self):
+       self.climberMotor.stopMotor()
+    
