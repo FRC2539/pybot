1bfe39b9e41f7d92270b55c2bf40018a284cbc87
Ben <bbistline02@gmail.com>
2021-01-09 11:25:31 -0500

    Fixed ports

--- ports.py
+++ ports.py
@@ -15,8 +15,8 @@ drivetrain = PortsList()
 
 '''CAN IDs for motors'''
 drivetrain.frontLeftMotorID = 1
-drivetrain.frontRightMotorID = 3
-drivetrain.backLeftMotorID = 2
+drivetrain.frontRightMotorID = 2
+drivetrain.backLeftMotorID = 3
 drivetrain.backRightMotorID = 4
 
 ColorWheelPorts = PortsList()
